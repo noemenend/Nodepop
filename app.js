8bb1f13e3032edd85a54a23a50c7325548ac22de
Noelia Muñiz Menéndez <nmunizmenend@gmail.com>
2018-07-18 12:45:54 +0200

    Añadiendo imágenes en la ruta /public/images/ y comentarios en app.js

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Middlewares
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
